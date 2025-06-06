3db049ced57ba023845bb6b99c85c0749cdc2cae
Adinath_Pawar <166931227+Adinath-S-Pawar@users.noreply.github.com>
2025-06-06 13:47:14 +0530

    Assignment16

---
+++ Assignment16/program5.c
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////////////////////////// 
+//
+// File name : program5.c
+// Description : Pattern
+// input : iRow = 4  iCol = 4
+// Output : 
+/*   
+   1    2   3   4
+        2   3   4
+            3   4
+                4
+    
+*/
+// Author : Adinath Santosh Pawar
+// Date : 30/5/25
+// 
+///////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int i = 0, j = 0;
+    if(iRow != iCol)
+    {
+        printf("Invalid input");
+        return;
+    }
+
+        for(i = 1 ; i <= iRow; i++)
+        {
+            for(j =  1 ;j <= iCol ; j++)
+            {
+                if(j >= i)
+                {
+                    printf("%d\t",j);
+                }
+                else
+                {
+                    printf("\t");
+                }
+                
+            }
+            printf("\n");
+        }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("enter number of rows \n");
+    scanf("%d",&iValue1);
+
+    printf("enter number of columns \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+    return 0;
+}
