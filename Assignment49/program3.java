6d631e8ba5ca11c1edebd1fa262a08b5c2f5ab06
Adinath_Pawar <166931227+Adinath-S-Pawar@users.noreply.github.com>
2025-10-16 20:21:12 +0530

    Assignment 49

---
+++ Assignment49/program3.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+/////////////////////////////////////////////////////////////////// 
+//
+// File name : program3.java
+// Description : Pattern printing
+// Author : Adinath Santosh Pawar
+// Date : 21/8/25           
+// 
+///////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public void Pattern(String str)
+    {
+        for(int i = 0; i < str.length(); i++)
+        {
+            for(int j = 0; j < str.length(); j++)
+            {
+                if(i >= j)
+                {
+                    System.out.print(str.charAt(j) + " ");
+                }
+                else
+                {
+                    System.out.print("# ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class program3
+{
+    public static void main(String A[])
+    {
+       Pattern pobj = new Pattern();
+       Scanner sobj = new Scanner(System.in);
+
+       System.out.print("Enter the string ");
+       String str = sobj.nextLine();
+
+       pobj.Pattern(str);
+    }
+}
